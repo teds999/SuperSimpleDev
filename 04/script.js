f89749483efe28719796778be492a7e2ccd840e5
KOT <tedoturmanidze12@gmail.com>
2025-03-17 16:51:55 +0300

    having problems with git

---
+++ 04/script.js
@@ -0,0 +1,10 @@
+let calculator =  2 + 2;
+console.log(calculator);
+console.log(calculator + 2);
+
+
+let result = calculator + 2;
+console.log(result);
+
+let message = 'hello';
+console.log(message);
